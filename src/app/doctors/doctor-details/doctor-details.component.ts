d9bb8256ef4052aa4245b879901c9ecf30f0f3e9
Omar Dalgamuni <omar@dalgamuni.me>
2022-01-29 17:47:33 +0100

    Styling

--- src/app/doctors/doctor-details/doctor-details.component.ts
+++ src/app/doctors/doctor-details/doctor-details.component.ts
@@ -12,6 +12,5 @@ export class DoctorDetailsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {    
-  }
-
+  }  
 }
--- src/app/models/doctor.model.ts
+++ src/app/models/doctor.model.ts
@@ -4,6 +4,7 @@ import { Company } from "./company.model";
 export interface Doctor{
     id: number,
     name: string,
+    username: string,
     email: string,
     address: Address,
     phone: string,
